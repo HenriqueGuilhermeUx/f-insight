b36a74f2c0cc83b2abca2c7b573f3f2cc2586872
HenriqueGuilhermeUx <henriquecampos66@gmail.com>
2026-07-03 12:21:23 -0300

    Wire launch admin and invite routes

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,12 @@ import MacroSignals from './pages/MacroSignals';
 import WhiteLabelSettings from './pages/WhiteLabelSettings';
 import ClientPortal from './pages/ClientPortal';
 import AdvisorWorkspace from './pages/AdvisorWorkspace';
+import RegisterOffice from './pages/RegisterOffice';
+import AdminDashboard from './pages/AdminDashboard';
+import AdminAdvisors from './pages/AdminAdvisors';
+import AdminClients from './pages/AdminClients';
+import AdminReports from './pages/AdminReports';
+import InvitePage from './pages/InvitePage';
 
 function App() {
   return (
@@ -29,6 +35,12 @@ function App() {
           <Route path="/white-label" element={<WhiteLabelSettings />} />
           <Route path="/assessor" element={<AdvisorWorkspace />} />
           <Route path="/cliente" element={<ClientPortal />} />
+          <Route path="/cadastro-escritorio" element={<RegisterOffice />} />
+          <Route path="/admin" element={<AdminDashboard />} />
+          <Route path="/admin/assessores" element={<AdminAdvisors />} />
+          <Route path="/admin/clientes" element={<AdminClients />} />
+          <Route path="/admin/relatorios" element={<AdminReports />} />
+          <Route path="/convite/:token" element={<InvitePage />} />
           <Route path="*" element={<Home />} />
         </Routes>
         <Toaster position="top-right" richColors />
