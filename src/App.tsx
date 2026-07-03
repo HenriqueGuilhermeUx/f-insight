43de779f115465afee408dd22ed8cbd79dfd4092
HenriqueGuilhermeUx <henriquecampos66@gmail.com>
2026-07-03 13:24:55 -0300

    Wire authentication provider and login route

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Toaster } from 'sonner';
 import { TenantProvider } from './context/TenantContext';
+import { AuthProvider } from './context/AuthContext';
 import Home from './pages/Home';
 import Radar from './pages/Radar';
 import AssetDetails from './pages/AssetDetails';
@@ -18,33 +19,37 @@ import AdminAdvisors from './pages/AdminAdvisors';
 import AdminClients from './pages/AdminClients';
 import AdminReports from './pages/AdminReports';
 import InvitePage from './pages/InvitePage';
+import Login from './pages/Login';
 
 function App() {
   return (
     <TenantProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/radar" element={<Radar />} />
-          <Route path="/ativo/:ticker" element={<AssetDetails />} />
-          <Route path="/watchlist" element={<Watchlist />} />
-          <Route path="/noticias" element={<News />} />
-          <Route path="/analises" element={<Analyses />} />
-          <Route path="/alertas" element={<Alerts />} />
-          <Route path="/macro" element={<MacroSignals />} />
-          <Route path="/white-label" element={<WhiteLabelSettings />} />
-          <Route path="/assessor" element={<AdvisorWorkspace />} />
-          <Route path="/cliente" element={<ClientPortal />} />
-          <Route path="/cadastro-escritorio" element={<RegisterOffice />} />
-          <Route path="/admin" element={<AdminDashboard />} />
-          <Route path="/admin/assessores" element={<AdminAdvisors />} />
-          <Route path="/admin/clientes" element={<AdminClients />} />
-          <Route path="/admin/relatorios" element={<AdminReports />} />
-          <Route path="/convite/:token" element={<InvitePage />} />
-          <Route path="*" element={<Home />} />
-        </Routes>
-        <Toaster position="top-right" richColors />
-      </BrowserRouter>
+      <AuthProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/radar" element={<Radar />} />
+            <Route path="/ativo/:ticker" element={<AssetDetails />} />
+            <Route path="/watchlist" element={<Watchlist />} />
+            <Route path="/noticias" element={<News />} />
+            <Route path="/analises" element={<Analyses />} />
+            <Route path="/alertas" element={<Alerts />} />
+            <Route path="/macro" element={<MacroSignals />} />
+            <Route path="/white-label" element={<WhiteLabelSettings />} />
+            <Route path="/assessor" element={<AdvisorWorkspace />} />
+            <Route path="/cliente" element={<ClientPortal />} />
+            <Route path="/cadastro-escritorio" element={<RegisterOffice />} />
+            <Route path="/admin" element={<AdminDashboard />} />
+            <Route path="/admin/assessores" element={<AdminAdvisors />} />
+            <Route path="/admin/clientes" element={<AdminClients />} />
+            <Route path="/admin/relatorios" element={<AdminReports />} />
+            <Route path="/convite/:token" element={<InvitePage />} />
+            <Route path="*" element={<Home />} />
+          </Routes>
+          <Toaster position="top-right" richColors />
+        </BrowserRouter>
+      </AuthProvider>
     </TenantProvider>
   );
 }
