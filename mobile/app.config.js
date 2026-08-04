6565a1997d7f7a898dee4cc3d93840fef2ef5647
HenriqueGuilhermeUx <henriquecampos66@gmail.com>
2026-08-04 13:04:30 -0300

    Add branded Android app icons

--- mobile/app.config.js
+++ mobile/app.config.js
@@ -2,22 +2,30 @@ module.exports = ({ config }) => ({
   ...config,
   name: 'F-Insight',
   slug: 'finsight-mobile',
-  version: '1.0.2',
+  version: '1.0.3',
   orientation: 'portrait',
   scheme: 'finsight',
   userInterfaceStyle: 'dark',
   platforms: ['android'],
   newArchEnabled: true,
+  icon: './assets/icon.png',
   updates: {
     enabled: false
   },
   splash: {
+    image: './assets/icon.png',
     resizeMode: 'contain',
     backgroundColor: '#020617'
   },
   android: {
     package: 'br.com.finsight.app',
-    versionCode: 3,
+    versionCode: 4,
+    icon: './assets/icon.png',
+    adaptiveIcon: {
+      foregroundImage: './assets/adaptive-icon.png',
+      monochromeImage: './assets/monochrome-icon.png',
+      backgroundColor: '#020617'
+    },
     permissions: ['INTERNET'],
     edgeToEdgeEnabled: true
   },
