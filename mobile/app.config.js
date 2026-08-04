bb49982ab00d246ce6cb68218e5c39b32a610407
HenriqueGuilhermeUx <henriquecampos66@gmail.com>
2026-08-03 22:45:46 -0300

    Disable mobile updates and bump Android version

--- mobile/app.config.js
+++ mobile/app.config.js
@@ -2,19 +2,22 @@ module.exports = ({ config }) => ({
   ...config,
   name: 'F-Insight',
   slug: 'finsight-mobile',
-  version: '1.0.0',
+  version: '1.0.1',
   orientation: 'portrait',
   scheme: 'finsight',
   userInterfaceStyle: 'dark',
   platforms: ['android'],
   newArchEnabled: true,
+  updates: {
+    enabled: false
+  },
   splash: {
     resizeMode: 'contain',
     backgroundColor: '#020617'
   },
   android: {
     package: 'br.com.finsight.app',
-    versionCode: 1,
+    versionCode: 2,
     permissions: ['INTERNET'],
     edgeToEdgeEnabled: true
   },
