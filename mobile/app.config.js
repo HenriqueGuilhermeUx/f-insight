9e0ea3e9e1a1330aa86ad9e8818cdbd401f44112
HenriqueGuilhermeUx <henriquecampos66@gmail.com>
2026-08-08 15:03:56 -0300

    Bump Android version for reviewer login build

--- mobile/app.config.js
+++ mobile/app.config.js
@@ -2,7 +2,7 @@ module.exports = ({ config }) => ({
   ...config,
   name: 'F-Insight',
   slug: 'finsight-mobile',
-  version: '1.0.8',
+  version: '1.0.9',
   orientation: 'portrait',
   scheme: 'finsight',
   userInterfaceStyle: 'dark',
@@ -18,7 +18,7 @@ module.exports = ({ config }) => ({
   },
   android: {
     package: 'br.com.finsight.app',
-    versionCode: 9,
+    versionCode: 10,
     permissions: ['INTERNET']
   },
   extra: {
