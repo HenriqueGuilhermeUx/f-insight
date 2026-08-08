6ca63aa0bd57959424bb346f7c499dcfa7468604
HenriqueGuilhermeUx <henriquecampos66@gmail.com>
2026-08-08 17:46:26 -0300

    Bump mobile version for Futuro IA MVP

--- mobile/app.config.js
+++ mobile/app.config.js
@@ -2,7 +2,7 @@ module.exports = ({ config }) => ({
   ...config,
   name: 'F-Insight',
   slug: 'finsight-mobile',
-  version: '1.0.12',
+  version: '1.0.13',
   orientation: 'portrait',
   scheme: 'finsight',
   userInterfaceStyle: 'dark',
@@ -18,7 +18,7 @@ module.exports = ({ config }) => ({
   },
   android: {
     package: 'br.com.finsight.app',
-    versionCode: 13,
+    versionCode: 14,
     permissions: ['INTERNET']
   },
   extra: {
