f85eb1e983c10e55f50bee8cc09b832d615abd1b
HenriqueGuilhermeUx <henriquecampos66@gmail.com>
2026-08-13 23:01:01 -0300

    Bump Android version for F-Insight Agent mobile build

--- mobile/app.config.js
+++ mobile/app.config.js
@@ -2,7 +2,7 @@ module.exports = ({ config }) => ({
   ...config,
   name: 'F-Insight',
   slug: 'finsight-mobile',
-  version: '1.0.13',
+  version: '1.0.14',
   orientation: 'portrait',
   scheme: 'finsight',
   userInterfaceStyle: 'dark',
@@ -18,7 +18,7 @@ module.exports = ({ config }) => ({
   },
   android: {
     package: 'br.com.finsight.app',
-    versionCode: 14,
+    versionCode: 15,
     permissions: ['INTERNET']
   },
   extra: {
